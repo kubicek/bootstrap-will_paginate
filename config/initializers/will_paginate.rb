a50186fd2a2a17311f8b7d9d703c5ea1576b1a31
Jiří Kubíček <jiri@kubicek.cz>
2012-02-13 03:35:00 +0100

    Added bs_ prefix to not interfere with will_paginate

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -7,12 +7,12 @@ require 'will_paginate/view_helpers/action_view'
 
 module WillPaginate
   module ActionView
-    def will_paginate(collection = nil, options = {})
+    def bs_will_paginate(collection = nil, options = {})
       options, collection = collection, nil if collection.is_a? Hash
       # Taken from original will_paginate code to handle if the helper is not passed a collection object.
       collection ||= infer_collection_from_controller 
       options[:renderer] ||= BootstrapLinkRenderer
-      super.try :html_safe
+      will_paginate(collection, options).try :html_safe
     end
 
     class BootstrapLinkRenderer < LinkRenderer
